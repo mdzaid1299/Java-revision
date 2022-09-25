05fcd78f4355207c14c61397502f1bc7f44f750f
Nilufa Khan <khannilufa5@gmail.com>
2022-09-26 00:07:20 +0530

    Initial commit

---
+++ src/arraypackage/ReverseString.java
@@ -0,0 +1,20 @@
+package arraypackage;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        ReverseString reverseString = new ReverseString();
+        System.out.println("reverseString = " + reverseString.reverse("Hello"));
+    }
+    // Hello ----- 0lleH
+    public String reverse(String str){
+        String response = "";
+//        char lastChar = str.charAt(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char lastChar = str.charAt(i);
+            response = lastChar + response;
+            lastChar--;
+        }
+        return response;
+    }
+}
+
