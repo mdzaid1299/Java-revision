d612d56b54799398588c1f4f82360c935d72c479
Nilufa Khan <khannilufa5@gmail.com>
2022-11-15 00:00:33 +0530

    initial

--- src/arraypackage/MoveAllNegativeNumbersBeginning.java
+++ src/arraypackage/MoveAllNegativeNumbersBeginning.java
@@ -4,6 +4,7 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class MoveAllNegativeNumbersBeginning {
+
     public static void main(String[] args) {
         MoveAllNegativeNumbersBeginning moveAllNegativeNumbersBeginning = new MoveAllNegativeNumbersBeginning();
         int[] arr = {-2,5,-9,5,-5,-8,0,-10,34,6};
