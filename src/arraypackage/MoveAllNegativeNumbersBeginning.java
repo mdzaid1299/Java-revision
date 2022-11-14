1119eae56fd6b7778bb498660568e42351f4d88c
Nilufa Khan <khannilufa5@gmail.com>
2022-11-15 00:00:52 +0530

    initial

--- src/arraypackage/MoveAllNegativeNumbersBeginning.java
+++ src/arraypackage/MoveAllNegativeNumbersBeginning.java
@@ -9,6 +9,7 @@ public class MoveAllNegativeNumbersBeginning {
         MoveAllNegativeNumbersBeginning moveAllNegativeNumbersBeginning = new MoveAllNegativeNumbersBeginning();
         int[] arr = {-2,5,-9,5,-5,-8,0,-10,34,6};
         moveAllNegativeNumbersBeginning.moveNegativeNumber(arr);
+
         System.out.println("After moving");
         for(int var : arr){
             System.out.print(var + " ");
