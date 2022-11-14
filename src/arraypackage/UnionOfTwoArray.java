8568f2ee45cacd719a99350e1c197d9cabc297fc
Nilufa Khan <khannilufa5@gmail.com>
2022-11-15 00:14:57 +0530

    initial

---
+++ src/arraypackage/UnionOfTwoArray.java
@@ -0,0 +1,28 @@
+package arraypackage;
+
+import java.util.HashSet;
+
+public class UnionOfTwoArray {
+    public static void main(String[] args) {
+        UnionOfTwoArray unionOfTwoArray = new UnionOfTwoArray();
+        int arr1[] = {4,2,1,8};
+        int[] arr2 = {2,5,6,1};
+        System.out.println("After union of two array ");
+        System.out.println(unionOfTwoArray.findUnion(arr1, arr2));
+
+
+
+    }
+    public HashSet<Integer> findUnion(int[] arr1, int[] arr2){
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i <arr1.length ; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i <arr2.length ; i++) {
+            set.add(arr2[i]);
+        }
+//        return set.size(); // output = 6
+        return set;
+    }
+}
