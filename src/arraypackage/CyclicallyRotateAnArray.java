ae521d30ddc7c82fa82917ebcebb96c5612635d8
Nilufa Khan <khannilufa5@gmail.com>
2022-11-16 08:38:48 +0530

    CyclicallyRotateAnArray

--- src/arraypackage/CyclicallyRotateAnArray.java
+++ src/arraypackage/CyclicallyRotateAnArray.java
@@ -17,6 +17,7 @@ public class CyclicallyRotateAnArray {
     public static void main(String[] args) {
 
         CyclicallyRotateAnArray cyclicallyRotateAnArray = new CyclicallyRotateAnArray();
+
         int[] arr = {1,2,3,4,5};
         int[] arr2 = {9, 8, 7, 6, 4, 2, 1, 3};
         System.out.println("Odd rotation");
