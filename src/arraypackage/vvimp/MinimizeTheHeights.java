63f30a02c24427f8aa8ea61fd964e6bc04062704
Nilufa Khan <khannilufa5@gmail.com>
2022-11-19 18:27:44 +0530

    Minimise the maximum difference between two heights

--- src/arraypackage/vvimp/MinimizeTheHeights.java
+++ src/arraypackage/vvimp/MinimizeTheHeights.java
@@ -1,7 +1,30 @@
 package arraypackage.vvimp;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.util.Arrays.sort;
+
 public class MinimizeTheHeights {
     public static void main(String[] args) {
+        int[] arr = {1, 15, 10};
+        MinimizeTheHeights minimizeTheHeights = new MinimizeTheHeights();
+        int minDiff = minimizeTheHeights.getMinimumDifference(arr, arr.length, 6);
+        System.out.println("minDiff = " + minDiff);
 
+    }
+    public int getMinimumDifference(int[] arr,int size, int k ){
+        sort(arr);
+        int initiallyMaxHeight = arr[size-1];
+        int initiallyMinHeight = arr[0];
+        int answer = initiallyMaxHeight - initiallyMinHeight;
+        for (int i = 1; i < arr.length; i++) {
+            initiallyMinHeight = min(arr[0] + k,arr[i] - k);
+            initiallyMaxHeight = max(arr[i-1] + k , arr[size-1] -k);
+            answer = min(answer,initiallyMaxHeight-initiallyMinHeight);
+        }
+        return answer;
     }
 }
