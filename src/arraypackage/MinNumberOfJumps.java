2c6cda370ced4479cb96ed162ad41693e9733307
Nilufa Khan <khannilufa5@gmail.com>
2022-11-19 19:04:35 +0530

    Binary Search

---
+++ src/arraypackage/MinNumberOfJumps.java
@@ -0,0 +1,14 @@
+package arraypackage;
+
+import static java.util.Arrays.sort;
+
+public class MinNumberOfJumps {
+    public static void main(String[] args) {
+
+    }
+    public int minNoOfJumpsToReachEndOfTheArray(int[] arr){
+        sort(arr);
+        // apply binary search logic
+       return 0;
+    }
+}
