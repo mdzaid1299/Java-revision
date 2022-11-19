61131d3a651c73ae3840fa1bb2a35fd37485364f
Nilufa Khan <khannilufa5@gmail.com>
2022-11-19 19:05:05 +0530

    Binary Search

--- src/arraypackage/BinarySearch.java
+++ src/arraypackage/BinarySearch.java
@@ -8,11 +8,14 @@ public class BinarySearch {
         binarySearch(arr,0,last,key);
     }
     public static void binarySearch(int arr[], int first, int last, int key){
+
         int mid = (first + last)/2;
         while( first <= last ){
+
             if ( arr[mid] < key ){
                 first = mid + 1;
             }else if ( arr[mid] == key ){
+
                 System.out.println("Element is found at index: " + mid);
                 break;
             }else{
