3f7c2e6b6505c2a2fa1d1a9be4880a8da902a430
Nilufa Khan <khannilufa5@gmail.com>
2022-11-23 20:51:35 +0530

    Has a relation

--- src/oops/inheritance/aggregate/AggregateRelation.java
+++ src/oops/inheritance/aggregate/AggregateRelation.java
@@ -2,6 +2,7 @@ package oops.inheritance.aggregate;
 
 public class AggregateRelation {
     public static void main(String[] args) {
+
         Address address = new Address("s12","kol","wb","Ind",22424);
         Employee employee = new Employee(12,"Nil","nig@gnam",address);
         System.out.println(employee);
