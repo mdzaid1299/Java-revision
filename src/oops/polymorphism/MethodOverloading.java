90f5b6a7cdf076e0b95c380c296df0cba9112785
Nilufa Khan <khannilufa5@gmail.com>
2022-11-23 21:06:46 +0530

    Method overloading

--- src/oops/polymorphism/MethodOverloading.java
+++ src/oops/polymorphism/MethodOverloading.java
@@ -1,20 +1,20 @@
 package oops.polymorphism;
 
 public class MethodOverloading {
-    public int iphoneCamera(int camera1){
-        return 1;
+    public int add(int num1){
+        return num1;
     }
-    public int iphoneCamera(int camera1,int camera2){
-        return 2;
+    public int add(int num,int num1){
+        return num+num1;
     }
-    public int iphoneCamera(int camera1,double camera2){
-        return 2;
+    public double add(int num,double num1){
+        return num+num1;
     }
     public static void main(String[] args) {
         MethodOverloading methodOverloading = new MethodOverloading();
-        System.out.println(methodOverloading.iphoneCamera(1));
-        System.out.println(methodOverloading.iphoneCamera(1, 4));
-        System.out.println(methodOverloading.iphoneCamera(1, 11));
+        System.out.println(methodOverloading.add(22));
+        System.out.println(methodOverloading.add(1, 4));
+        System.out.println(methodOverloading.add(1, 11.55));
 
     }
 }
