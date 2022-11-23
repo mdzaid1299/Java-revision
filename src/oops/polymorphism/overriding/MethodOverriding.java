2b0e719eca306b4c30f21ad7e89cdcfcf69a1119
Nilufa Khan <khannilufa5@gmail.com>
2022-11-24 00:08:26 +0530

    Method overriding

--- src/oops/polymorphism/overriding/MethodOverriding.java
+++ src/oops/polymorphism/overriding/MethodOverriding.java
@@ -1,6 +1,7 @@
 package oops.polymorphism.overriding;
 
 public class MethodOverriding {
+
     public static void main(String[] args) {
         Bike bike = new Bike();
         Car car = new Car();
