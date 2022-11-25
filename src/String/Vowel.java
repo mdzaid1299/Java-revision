c94e3f4ddfe23519c019e9301626e1eeabb53b4c
HP <mdzaid1299@gmail.com>
2022-11-26 02:49:10 +0530

    ini error

---
+++ src/String/Vowel.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Scanner;
+
+public class Vowel {
+    //Write a Java program to check if a vowel is present in a string.
+    //=================================================================
+    //
+    //ip:  Hi Welcome to my world!"
+    //op: iska ulta ok
+    //
+    //1.take input string from user.
+    //2.take a for loop from 0 to string length
+    //3.take if condition check str.charAt(i) == 'a'|| like that
+    //print the vowel and also print its index.
+    Scanner scanner = new Scanner(System.in);
+    String isVoWel = scanner.nextLine();
+
+      System.out.println("Characters in " + name + " are:");
+
+}
