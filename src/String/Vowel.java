047d5029dc69d228b637ab83eeb6e2eed87ff1da
HP <mdzaid1299@gmail.com>
2022-11-26 21:33:51 +0530

    ini

--- src/String/Vowel.java
+++ src/String/Vowel.java
@@ -13,9 +13,21 @@ public class Vowel {
     //2.take a for loop from 0 to string length
     //3.take if condition check str.charAt(i) == 'a'|| like that
     //print the vowel and also print its index.
+    public static void main(String[] args) {
+
     Scanner scanner = new Scanner(System.in);
     String isVoWel = scanner.nextLine();
+        System.out.println("isVoWel original= " + isVoWel);
+    String rst= "";
+    char ch;
+        for (int i = 0; i < isVoWel.length(); i++) {
+            ch = isVoWel.charAt(i);
+            rst = ch + rst;
+        System.out.println("index " + i);
+        }
+        System.out.println("reversed " + rst);
+    }
 
-      System.out.println("Characters in " + name + " are:");
+
 
 }
