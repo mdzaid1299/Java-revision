a623427c1ebaff4f621951fad5783e8d30e03ef9
HP <mdzaid1299@gmail.com>
2022-11-27 14:35:24 +0530

    ini

---
+++ src/String/MyClass.java
@@ -0,0 +1,23 @@
+package String;
+
+public class MyClass {
+
+    String name;
+    int rollno;
+    static String college = "ICT";
+    MyClass(String name, int rollno){
+        this.name = name;
+        this.rollno = rollno;
+    }
+    void display (){
+        System.out.println(rollno + " "+ name + "  " + college );
+    }
+    public static class School{
+        public static void main(String[] args) {
+            MyClass obj1 = new MyClass("Jojo",227);
+            MyClass obj2 = new MyClass("Sukura" , 765);
+            obj1.display();
+            obj2.display();
+        }
+    }
+}
--- src/String/Vowel.java
+++ src/String/Vowel.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Vowel {
     //Write a Java program to check if a vowel is present in a string.
     //=================================================================
-    //
+
     //ip:  Hi Welcome to my world!"
     //op: iska ulta ok
-    //
     //1.take input string from user.
     //2.take a for loop from 0 to string length
     //3.take if condition check str.charAt(i) == 'a'|| like that
