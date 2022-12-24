345fcfe036d9e2bbe789010ee0ac91afd87d8dbb
HP <mdzaid1299@gmail.com>
2022-12-25 00:13:17 +0530

    ini-transversing-array

---
+++ src/ArraysHusb/basicArray.java
@@ -0,0 +1,13 @@
+package ArraysHusb;
+
+public class basicArray {
+    public static void main(String[] args) {
+
+
+        int[] age = {56, 75, 97, 32, 85};
+   for (int i = 0 ; i < age.length ; i++)
+        {
+            System.out.println(age[i]);
+        }
+    }
+}
