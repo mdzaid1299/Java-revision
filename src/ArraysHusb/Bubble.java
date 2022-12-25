9e25a7410a17c2b72d99e00383de13de10c4cf64
HP <mdzaid1299@gmail.com>
2022-12-26 00:07:45 +0530

    ini-B sort

--- src/ArraysHusb/Bubble.java
+++ src/ArraysHusb/Bubble.java
@@ -2,6 +2,17 @@ package ArraysHusb;
 
 public class Bubble {
     public static void main(String[] args) {
-        System.out.println("bubble");
+        int[] a = {78, 85, 34, 75};
+        int temp ;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; i < a.length - 1 -i; i++) {
+                if (a[j] > a[j + 1]) {
+                  temp = a [j];
+                    a[j] = a[j + 1];
+                    a [j + 1 ] = temp;
+                }
+            } System.out.println(a);
+
+        }
     }
 }
