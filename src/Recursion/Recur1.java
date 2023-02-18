25d5b3eda85a6368c893f552a6d8dfce5b5ea004
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-02-19 00:07:17 +0530

    ini-d

---
+++ src/Recursion/Recur1.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Recur1 {
+    public static void main(String[] args) {
+        Recur1 rec = new Recur1();
+        int mac =rec.fact(3);
+        System.out.println(mac);
+    }
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }else {
+            return n * fact(n - 1);
+        }
+    }
+}
