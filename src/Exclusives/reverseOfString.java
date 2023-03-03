4ca26c0420d0b82818bda975076ce6053e4b1419
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-03-03 08:30:41 +0530

    ini-d

--- src/Exclusives/reverseOfString.java
+++ src/Exclusives/reverseOfString.java
@@ -16,3 +16,10 @@ public class reverseOfString {
         scanner.close();
     }
 }
+/**
+ * builder.reverse(); is a method of the StringBuilder class that reverses the sequence of characters in the StringBuilder object. This means that the original string in the StringBuilder object is now reversed.
+ *
+ * builder.toString(); is a method of the StringBuilder class that returns a String object representing the data in the StringBuilder object. In this case, it returns a new String object containing the reversed sequence of characters from the original StringBuilder object.
+ *
+ * These lines are used in the given code to reverse the input string using the StringBuilder class, and store the reversed string in a new variable reversed.
+ */
