84af40182ca07efd95c5303e627ef4f118875934
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-03-13 23:23:31 +0530

    ini-d

--- src/topgs/FindLargestAndSmallestFromArray.java
+++ src/topgs/FindLargestAndSmallestFromArray.java
@@ -1,13 +1,13 @@
 package topgs;
 
-public class findLargestAndSmallestFromArray {
+public class FindLargestAndSmallestFromArray {
     public static void main(String[] args) {
         int number[] = {83, 54, 21, 78, 46};
         int largest = number[0];
         int smallest = number[0];
 
         for (int i = 0; i < number.length; i++) {
-            if (number[i] > largest) {
+            if (number[i] >  largest) {
                 largest = number[i];
             } else if (number[i] < smallest) {
                 smallest = number[i];
