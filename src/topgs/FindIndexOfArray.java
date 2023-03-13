795d5645e02ff3c83eb3880e99e7b5366e4d153d
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-03-14 00:23:36 +0530

    ini-d

---
+++ src/topgs/FindIndexOfArray.java
@@ -0,0 +1,30 @@
+package topgs;
+
+import java.util.Scanner;
+
+public class FindIndexOfArray {
+    public static void main(String[] args) {
+        int[] array = {8, 44, 2323, 66, 78, 2, 76};
+        System.out.println("enter the element you want index of");
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        int index = findIndex(array, target);
+        if (index == -1) {
+            System.out.println("not found");
+        } else {
+            System.out.println("index of " + target + " in array is " + index);
+        }
+    }
+
+    public static int findIndex(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return 1;
+            }
+
+        }
+        return -1;
+    }
+
+
+}
