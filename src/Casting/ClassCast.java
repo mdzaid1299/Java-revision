05261200472b0fdbeb525ed662345d21de4830f8
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-05-23 21:21:35 +0530

    ini-f part 19

---
+++ src/Casting/ClassCast.java
@@ -0,0 +1,13 @@
+package Casting;
+
+public class ClassCast {
+    public static void main(String[] args) {
+        String string = "10";
+        int num = Integer.parseInt(string);
+        System.out.println(num);
+
+        String string1 = "java";
+        int num2 = Integer.parseInt(string1);
+        System.out.println("num2 = " + num2);
+    }
+}
