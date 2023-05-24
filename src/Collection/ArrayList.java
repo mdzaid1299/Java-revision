12807540bb85e221e26062e6c1c9a75364334f2b
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-05-24 20:41:38 +0530

    ini-f part 22

---
+++ src/Collection/ArrayList.java
@@ -0,0 +1,16 @@
+package Collection;
+
+import java.util.*;
+import java.util.Enumeration;
+import java.util.List;
+
+public class ArrayList  {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(5);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+}
